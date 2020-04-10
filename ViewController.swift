a11ccd5aefcb3fb49e264031fe215d3a6cec07d0
Taolue <1164734648@qq.com>
2020-04-10 22:32:38 +0800

    aaa

--- ViewController.swift
+++ ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
 		// setup locationManager
 		locationManager.delegate = self;
 		locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters;
-		locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+//		locationManager.desiredAccuracy = kCLLocationAccuracyBest;
 
 		// setup mapView
 		mapView.delegate = self
